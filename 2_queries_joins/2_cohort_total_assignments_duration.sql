18a9957dfd44407aa43b671f4574ad1ccfe75ace
Taha Helwani <telhe008@uottawa.ca>
2021-11-23 20:16:41 +0000

    Implemented the rollover students query as well as the Student's Total Assignment Duration query and the cohort's total assignment duration query

---
+++ 2_queries_joins/2_cohort_total_assignments_duration.sql
@@ -0,0 +1,11 @@
+SELECT SUM(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+where cohorts.name = 'FEB12';
+
+-- SELECT *
+-- FROM assignment_submissions
+-- JOIN students ON student_id = students.id
+-- JOIN cohorts ON students.cohort_id = cohorts.id
+-- WHERE cohorts.name = 'FEB12';
